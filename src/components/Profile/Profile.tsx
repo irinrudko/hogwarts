5edf803455cf3afb9f090cfd9b33a2c0782f910d
Iryna Rudzko <irynarudzko@MacBook-Air-Iryna.local>
2021-07-06 13:03:10 +0300

    refactor: rmove ProfileInfo from Profile component to a special folder

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
 import style from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts';
-
-const ProfilePic = () => {
-    return (
-        <>
-            <div>
-                <img src='https://images.unsplash.com/photo-1588392382834-a891154bca4d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1355&q=80' />
-            </div>
-
-            <div>
-                ava + descr
-            </div>
-        </>
-    )
-}
+import ProfileInfo from './ProfileInfo/ProfileInfo';
 
 const Profile = () => {
     return (
         <main className={style.profile}>
-
-            <ProfilePic />
+            <ProfileInfo />
             <MyPosts />
         </main>
     )
