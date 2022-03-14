32360f5f7800e716607f33b95eebd292edb1afeb
Iryna Rudzko <irinrudko@gmail.com>
2022-03-14 19:40:23 +0300

    feat: cover profile-reducer.ts with tests

--- src/redux/profile-reducer.test.ts
+++ src/redux/profile-reducer.test.ts
@@ -5,7 +5,7 @@ import { PostType, ProfilePageType } from "./redux";
 test('correct post text should be added', () => {
     let newPostMessage = 'new mes';
     const startState: ProfilePageType = {
-        postText: newPostMessage,
+        postText: '',
         postsData:
             [
                 { id: v1(), message: 'hey buddy!', likesCount: 0 },
@@ -17,4 +17,19 @@ test('correct post text should be added', () => {
 
     expect(endState.postsData.length).toBe(3);
     expect(endState.postsData[2].message).toBe(newPostMessage)
+})
+
+test('correct post should change its name', () => {
+    let newPostText = 'this is something new'
+    const startState: ProfilePageType = {
+        postText: '',
+        postsData:
+            [
+                { id: v1(), message: 'hey buddy!', likesCount: 0 },
+                { id: v1(), message: 'how is it going?', likesCount: 0 }
+            ]
+    }
+
+    const endState = profileReducer(startState, { type: 'CHANGE-POST-TEXT', newText: newPostText })
+
 })
