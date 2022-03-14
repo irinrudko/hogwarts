bdae96c261cdd17aa85861b1f70de553f6ff9369
Iryna Rudzko <irinrudko@gmail.com>
2022-03-14 21:13:38 +0300

    feat: add UPDATE-NEW-MESSAGE-BODY test for dialogs-reducer.ts

--- src/redux/dialogs-reducer.test.ts
+++ src/redux/dialogs-reducer.test.ts
@@ -5,8 +5,8 @@ import { DialogPageType } from "./redux"
 test('correct message should be sent', () => {
     const startState: DialogPageType = {
         dialogsData: [
-            { id: v1(), name: "Nastya", },
-            { id: v1(), name: "Tanya", },
+            { id: v1(), name: "Lena", },
+            { id: v1(), name: "Dima", },
         ],
         messagesData: [
             { id: v1(), message: "hey", },
@@ -15,10 +15,29 @@ test('correct message should be sent', () => {
         messageText: '',
     }
 
-    let newMessageText = 'hey, this is a new message!'
+    const newMessageText = 'hey, this is a new message!'
 
     const endState = dialogsReducer(startState, { type: 'SEND-MESSAGE', messageText: newMessageText })
 
     expect(endState.messagesData.length).toBe(3)
     expect(endState.messagesData[2].message).toBe(newMessageText)
+})
+
+test('message text for dialogs should be updated in State ', () => {
+    const newMessageText = 'hey, this is a new message text!'
+
+    const startState: DialogPageType = {
+        dialogsData: [
+            { id: v1(), name: "Lena", },
+            { id: v1(), name: "Dima", },
+        ],
+        messagesData: [
+            { id: v1(), message: "hey", },
+            { id: v1(), message: "how're you?", },
+        ],
+        messageText: '',
+    }
+
+    const endState = dialogsReducer(startState, { type: 'UPDATE-NEW-MESSAGE-BODY', messageBody: newMessageText })
+    expect(endState.messageText).toBe(newMessageText)
 })
