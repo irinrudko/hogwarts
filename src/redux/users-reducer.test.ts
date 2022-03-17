556c435b649f5a58517143f2f3b57996da426274
Iryna Rudzko <irinrudko@gmail.com>
2022-03-17 13:06:25 +0300

    feat: add UNFOLLOW-USER test for users-reducer

--- src/redux/users-reducer.test.ts
+++ src/redux/users-reducer.test.ts
@@ -20,3 +20,23 @@ test('should follow the correct user', () => {
     expect(endState.users[0].followed).toBe(true)
     expect(endState.users[1].followed).toBe(false)
 })
+
+
+test('should unfollow the correct user', () => {
+    let _id = 1;
+
+    const startState: UsersPageType = {
+        users: [
+            { name: 'Shubert', id: _id, photos: { small: '', large: '' }, status: 'Hey guys', followed: true },
+            { name: 'Mozart', id: 2, photos: { small: '', large: '' }, status: 'Salieri seems to be a nice guy', followed: false },
+            { name: 'Beethoven', id: 3, photos: { small: '', large: '' }, status: 'Guys, I cannot hear anything!!', followed: false },
+        ]
+    }
+
+    const endState = usersReducer(startState, { type: 'UNFOLLOW-USER', userId: _id })
+
+    expect(endState.users[0].id).toBe(1)
+    expect(endState.users[0].followed).toBe(false)
+    expect(endState.users[1].followed).toBe(false)
+    expect(endState.users[2].followed).toBe(false)
+})
