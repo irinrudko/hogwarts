289e35eebc4b1d0be70bc177d180b60091d50b6a
Iryna Rudzko <irinrudko@gmail.com>
2022-03-17 13:15:27 +0300

    refactor: refactor tests

--- src/redux/users-reducer.test.ts
+++ src/redux/users-reducer.test.ts
@@ -2,23 +2,23 @@ import { UsersPageType } from "./redux"
 import { usersReducer } from "./users-reducer"
 
 test('should follow the correct user', () => {
-    let _id = 3;
+    let userId = 3;
 
     const startState: UsersPageType = {
         users: [
             { name: 'Shubert', id: 1, photos: { small: '', large: '' }, status: 'Hey guys', followed: true },
             { name: 'Mozart', id: 2, photos: { small: '', large: '' }, status: 'Salieri seems to be a nice guy', followed: false },
-            { name: 'Beethoven', id: _id, photos: { small: '', large: '' }, status: 'Guys, I cannot hear anything!!', followed: false },
+            { name: 'Beethoven', id: userId, photos: { small: '', large: '' }, status: 'Guys, I cannot hear anything!!', followed: false },
         ]
     }
 
-    const endState = usersReducer(startState, { type: 'FOLLOW-USER', userId: _id })
+    const endState = usersReducer(startState, { type: 'FOLLOW-USER', userId })
 
     expect(endState.users[2].id).toBe(3)
-    expect(endState.users[2].followed).toBe(true)
+    expect(endState.users[2].followed).toBeTruthy
     //проверка, что в другом месте нигде не полетело
-    expect(endState.users[0].followed).toBe(true)
-    expect(endState.users[1].followed).toBe(false)
+    expect(endState.users[0].followed).toBeTruthy
+    expect(endState.users[1].followed).toBeFalsy
 })
 
 
@@ -36,7 +36,7 @@ test('should unfollow the correct user', () => {
     const endState = usersReducer(startState, { type: 'UNFOLLOW-USER', userId: _id })
 
     expect(endState.users[0].id).toBe(1)
-    expect(endState.users[0].followed).toBe(false)
-    expect(endState.users[1].followed).toBe(false)
-    expect(endState.users[2].followed).toBe(false)
+    expect(endState.users[0].followed).toBeFalsy
+    expect(endState.users[1].followed).toBeFalsy
+    expect(endState.users[2].followed).toBeFalsy
 })
