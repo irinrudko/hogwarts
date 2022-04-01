3bb407fb143d54f227a24412a074da17eb76b5f6
Iryna Rudzko <irinrudko@gmail.com>
2022-04-01 19:11:02 +0300

    refactor: add new values to startState in users-reducer tests

--- src/redux/users-reducer.test.ts
+++ src/redux/users-reducer.test.ts
@@ -12,7 +12,8 @@ it('should follow the correct user', () => {
         totalUsersCount: 20,
         pageSize: 5,
         currentPage: 2,
-        isFetching: false
+        isFetching: false,
+        isFollowingInProgress: []
     }
 
     const endState = usersReducer(startState, { type: 'FOLLOW-USER', userId })
@@ -37,7 +38,8 @@ it('should unfollow the correct user', () => {
         totalUsersCount: 20,
         pageSize: 5,
         currentPage: 2,
-        isFetching: false
+        isFetching: false,
+        isFollowingInProgress: []
     }
 
     const endState = usersReducer(startState, { type: 'UNFOLLOW-USER', userId: _id })
