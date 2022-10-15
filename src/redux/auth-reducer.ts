22a84bcc52bf44fd257332bc061bd977d6223994
Iryna Rudzko <irinrudko@gmail.com>
2022-10-15 22:49:44 +0400

    chore: format auth-reducer

--- src/redux/auth-reducer.ts
+++ src/redux/auth-reducer.ts
@@ -1,26 +1,26 @@
-import { authAPI } from "./API/api"
-import { AppThunk } from "./redux-store"
+import { authAPI } from './API/api'
+import { AppThunk } from './redux-store'
 
 const initialState: AuthUserType = {
     messages: [],
     data: {
         id: 2,
         email: 'blabla@bla.bla',
-        login: 'samurai'
+        login: 'samurai',
     },
-    isAuth: false
+    isAuth: false,
 }
 
 export const authReducer = (state: AuthUserType = initialState, action: AuthActionTypes): AuthUserType => {
-
     switch (action.type) {
         case 'SET_USER_DATA':
             return {
                 ...state,
                 data: { ...action.data },
-                isAuth: true
+                isAuth: true,
             }
-        default: return state
+        default:
+            return state
     }
 }
 
@@ -29,16 +29,13 @@ export type AuthActionTypes = ReturnType<typeof setUserDataAC>
 export const setUserDataAC = (id: number, email: string, login: string) => {
     return {
         type: 'SET_USER_DATA',
-        data: { id, email, login }
+        data: { id, email, login },
     } as const
 }
 
-
-
 export const authMe = (): AppThunk => {
     return (dispatch) => {
-
-        authAPI.me().then(data => {
+        authAPI.me().then((data) => {
             if (data.resultCode === 0) {
                 let { id, email, login } = data.data
                 dispatch(setUserDataAC(id, email, login))
@@ -46,9 +43,6 @@ export const authMe = (): AppThunk => {
         })
     }
 }
-
-
-
 
 export type AuthUserType = {
     messages: []
