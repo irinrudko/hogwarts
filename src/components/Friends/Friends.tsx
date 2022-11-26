59670946f6e429e9ca2b3278f5dd0d8ea6efb2fe
Iryna Rudzko <irinrudko@gmail.com>
2022-11-26 17:55:42 +0400

    feat: add friends to Friends page

--- src/components/Friends/Friends.tsx
+++ src/components/Friends/Friends.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
-import { compose } from 'redux';
-import { withAuthRedirect } from '../hoc/withAuthReducer';
-import style from './Friends.module.css';
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { compose } from 'redux'
+import { ReduxStateType } from '../../redux/redux-store'
+import { UserType } from '../../redux/users-reducer'
+import { withAuthRedirect } from '../hoc/withAuthReducer'
+import style from './Friends.module.css'
+import userPhoto from '../../assets/icons/avatar-male.png'
 
 const Friends = () => {
+    const users = useSelector<ReduxStateType, UserType[]>((state) => state.usersPage.users)
+    const myFriends = users.filter((user) => (user.followed ? user.name : null))
+
     return (
         <div className={style.item}>
-            Friends
+            {myFriends.map((u) => {
+                return (
+                    <div key={u.id}>
+                        <img src={u.photos.small !== null ? u.photos.small : userPhoto} alt="avatar" width={'70px'} />
+                        <span>{u.name}</span>
+                    </div>
+                )
+            })}
         </div>
     )
 }
