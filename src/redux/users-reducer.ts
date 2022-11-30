2b6e4da66c20e4dcf341ba50d207af86b2bb8c57
Iryna Rudzko <irinrudko@gmail.com>
2022-11-30 11:23:50 +0400

    fix: pagination

--- src/redux/users-reducer.ts
+++ src/redux/users-reducer.ts
@@ -1,110 +1,121 @@
-import { usersAPI } from "./API/api";
-import { AppThunk } from "./redux-store";
+import { usersAPI } from './API/api'
+import { AppThunk } from './redux-store'
 
 const initialState: UsersPageType = {
     users: [],
     totalUsersCount: 0,
-    pageSize: 100,
+    pageSize: 10,
     currentPage: 1,
     isFetching: false,
     isFollowingInProgress: [],
-};
+}
 
 export const usersReducer = (state: UsersPageType = initialState, action: UsersActionTypes): UsersPageType => {
     switch (action.type) {
         case 'FOLLOW-USER':
             return {
                 ...state,
-                users: state.users.map(u => {
+                users: state.users.map((u) => {
                     if (u.id === action.userId && u.followed === false) {
                         return { ...u, followed: true }
                     }
                     return u
-                })
+                }),
             }
         case 'UNFOLLOW-USER':
             return {
                 ...state,
-                users: state.users.map(u => {
+                users: state.users.map((u) => {
                     if (u.id === action.userId && u.followed === true) {
                         return { ...u, followed: false }
                     }
                     return u
-                })
+                }),
             }
         case 'SET-USERS':
             return {
-                ...state, users: [...state.users, ...action.users]
+                ...state,
+                users: action.users,
             }
         case 'SET-CURRENT-PAGE':
             return {
-                ...state, currentPage: action.currentPage
+                ...state,
+                currentPage: action.currentPage,
             }
         case 'SET-TOTAL-USERS-COUNT':
             return {
-                ...state, totalUsersCount: action.totalUsersCount
+                ...state,
+                totalUsersCount: action.totalUsersCount,
             }
         case 'TOGGLE-IS-FETCHING':
             return {
-                ...state, isFetching: action.isFetching
+                ...state,
+                isFetching: action.isFetching,
             }
         case 'TOGGLE-FOLLOWING-PROGRESS':
             return {
-                ...state, isFollowingInProgress: action.isFollowingInProgress
+                ...state,
+                isFollowingInProgress: action.isFollowingInProgress
                     ? [...state.isFollowingInProgress, action.userId]
-                    : state.isFollowingInProgress.filter(id => id !== action.userId)
+                    : state.isFollowingInProgress.filter((id) => id !== action.userId),
             }
-        default: return state
+        default:
+            return state
     }
 }
 
-export type UsersActionTypes = ReturnType<typeof followUserAC> | ReturnType<typeof unfollowUserAC> | ReturnType<typeof setUsersAC> | ReturnType<typeof setCurrentPageAC> | ReturnType<typeof setTotalUsersCountAC> | ReturnType<typeof toggleIsFetchingAC> | ReturnType<typeof toggleFollowingProgressAC>
+export type UsersActionTypes =
+    | ReturnType<typeof followUserAC>
+    | ReturnType<typeof unfollowUserAC>
+    | ReturnType<typeof setUsersAC>
+    | ReturnType<typeof setCurrentPageAC>
+    | ReturnType<typeof setTotalUsersCountAC>
+    | ReturnType<typeof toggleIsFetchingAC>
+    | ReturnType<typeof toggleFollowingProgressAC>
 
 export const followUserAC = (userId: number) => {
     return {
         type: 'FOLLOW-USER',
-        userId: userId
+        userId: userId,
     } as const
 }
 export const unfollowUserAC = (userId: number) => {
     return {
         type: 'UNFOLLOW-USER',
-        userId: userId
+        userId: userId,
     } as const
 }
 export const setUsersAC = (users: UserType[]) => {
     return {
         type: 'SET-USERS',
-        users: users
+        users: users,
     } as const
 }
 export const setCurrentPageAC = (currentPage: number) => {
     return {
         type: 'SET-CURRENT-PAGE',
-        currentPage
+        currentPage,
     } as const
 }
 export const setTotalUsersCountAC = (totalUsersCount: number) => {
     return {
         type: 'SET-TOTAL-USERS-COUNT',
-        totalUsersCount
+        totalUsersCount,
     } as const
 }
 export const toggleIsFetchingAC = (isFetching: boolean) => {
     return {
         type: 'TOGGLE-IS-FETCHING',
-        isFetching
+        isFetching,
     } as const
 }
 export const toggleFollowingProgressAC = (userId: number, isFollowingInProgress: boolean) => {
     return {
         type: 'TOGGLE-FOLLOWING-PROGRESS',
         userId,
-        isFollowingInProgress
+        isFollowingInProgress,
     } as const
 }
-
-
 
 /////
 //Thunks
@@ -113,7 +124,7 @@ export const getUsersTH = (pageNumber: number, pageSize: number): AppThunk => {
         dispatch(toggleIsFetchingAC(true))
         dispatch(setCurrentPageAC(pageNumber))
 
-        usersAPI.getUsers(pageNumber, pageSize).then(data => {
+        usersAPI.getUsers(pageNumber, pageSize).then((data) => {
             dispatch(setUsersAC(data.items))
             dispatch(toggleIsFetchingAC(false))
         })
@@ -123,7 +134,7 @@ export const followUser = (userId: number): AppThunk => {
     return (dispatch) => {
         dispatch(toggleFollowingProgressAC(userId, true))
 
-        usersAPI.followUser(userId).then(data => {
+        usersAPI.followUser(userId).then((data) => {
             if (data.resultCode === 0) {
                 dispatch(followUserAC(userId))
             }
@@ -135,7 +146,7 @@ export const unfollowUser = (userId: number): AppThunk => {
     return (dispatch) => {
         dispatch(toggleFollowingProgressAC(userId, true))
 
-        usersAPI.unfollowUser(userId).then(data => {
+        usersAPI.unfollowUser(userId).then((data) => {
             if (data.resultCode === 0) {
                 dispatch(unfollowUserAC(userId))
             }
@@ -146,9 +157,6 @@ export const unfollowUser = (userId: number): AppThunk => {
 
 //Thunks
 /////
-
-
-
 
 export type UsersPageType = {
     users: Array<UserType>
